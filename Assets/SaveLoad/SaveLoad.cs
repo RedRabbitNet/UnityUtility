8fe782f79764dc30c4739e827f972f70ecd15693
RedRabbitNet <redrabbitnet@gmail.com>
2020-01-18 18:27:10 +0900

    SaveLoad
    JsonUtilityを利用した静的クラスを作成しました。

---
+++ Assets/SaveLoad/SaveLoad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+/// <summary>
+/// SaveLoadに関する機能をまとめたクラス
+/// JsonUtilityを扱うため、保存されるデータはpublicまたはSerializeFieldであることに注意。
+/// </summary>
+static public class SaveLoad
+{
+	static public void Save<Type>(string fileName, Type data)
+	{
+		Debug.Log("Save:" + fileName);
+		File.WriteAllText(Application.streamingAssetsPath + "/" + fileName, JsonUtility.ToJson(data, true));
+	}
+
+	static public Type Load<Type>(string fileName)
+	{
+		Debug.Log("Load:" + fileName);
+		return JsonUtility.FromJson<Type>(File.ReadAllText(Application.streamingAssetsPath + "/" + fileName));
+	}
+}
---
+++ Assets/SaveLoad/SaveLoadSample.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadSample : MonoBehaviour {
+
+	[System.Serializable]
+	public class SampleData
+	{
+		public int virtualCoin;
+	}
+	private SampleData data;
+	const string fileName = "SampleData.json";
+
+	// Use this for initialization
+	void Start () {
+		data = SaveLoad.Load<SampleData>(fileName);
+		//data = new SampleData();
+		Debug.Log("virtualCoin:" + data.virtualCoin);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			data.virtualCoin++;
+			Debug.Log("virtualCoin:" + data.virtualCoin);
+		}
+
+		if (Input.GetKeyDown(KeyCode.S))
+			SaveLoad.Save<SampleData>(fileName, data);
+	}
+}
