a84acd3707ba3e2db5420eb2f419f8370caecef0
RedRabbitNet <redrabbitnet@gmail.com>
2020-01-27 22:30:04 +0900

    FocusButton
    特定の１つのボタン以外を操作不能にするFocusButtonを実装しました。

---
+++ Assets/UIFocus/script/FocusButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FocusButton : Button{
+	public void Focus()
+	{
+		foreach (Selectable selectableUI in Selectable.allSelectables)
+		{
+			//コンポーネントがアタッチされているオブジェクト以外のUI要素を無効にする
+			if (selectableUI.gameObject != this.gameObject)
+			{
+				selectableUI.interactable = false;
+				Debug.Log(selectableUI.name);
+			}
+			else
+			{
+				selectableUI.interactable = true;
+			}
+		}
+	}
+}
---
+++ Assets/UIFocus/script/FocusSample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusSample : MonoBehaviour {
+	[SerializeField] private FocusButton focusButton;
+
+	// Use this for initialization
+	void Start () {
+		//１つのUIを除いて選択不能にする
+		focusButton.Focus();
+	}
+	
+	//ボタン動作確認用
+	public void ClickLog()
+	{
+		Debug.Log("OnClick");
+	}
+}
